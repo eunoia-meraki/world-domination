025870a5fd46f99435d2c8a08d5f29c3c8b9873f
Aleksey Koren <alexey.koran@gmail.com>
2022-02-08 15:11:41 +0200

    Fix babel config

--- webapp/babel.config.ts
+++ webapp/babel.config.ts
@@ -1,44 +1,42 @@
 import type { ConfigAPI, TransformOptions } from '@babel/core';
 
-const config = (api: ConfigAPI): TransformOptions => {
-  return {
-    presets: ['@babel/preset-env'],
+const config = (api: ConfigAPI): TransformOptions => ({
+  presets: ['@babel/preset-env'],
 
-    overrides: [
-      {
-        test: /\.(m?ts|m?tsx)$/,
-        presets: ['@babel/preset-typescript'],
-      },
-      {
-        test: /\.(m?ts|m?tsx)$/,
-        presets: [
-          [
-            '@babel/preset-react',
-            {
-              development: api.env() === 'development',
-              useBuiltIns: true,
-              runtime: 'automatic',
-            },
-          ],
-        ],
-      },
-      {
-        test: '**/*.d.ts',
-        presets: [['@babel/preset-env', { targets: { esmodules: true } }]],
-      },
-    ],
-
-    plugins: [
-      [
-        'module-resolver',
-        {
-          alias: {
-            '@/': 'src/',
+  overrides: [
+    {
+      test: /\.(m?ts|m?tsx)$/,
+      presets: ['@babel/preset-typescript'],
+    },
+    {
+      test: /\.(m?ts|m?tsx)$/,
+      presets: [
+        [
+          '@babel/preset-react',
+          {
+            development: api.env() === 'development',
+            useBuiltIns: true,
+            runtime: 'automatic',
           },
-        },
+        ],
       ],
+    },
+    {
+      test: '**/*.d.ts',
+      presets: [['@babel/preset-env', { targets: { esmodules: true } }]],
+    },
+  ],
+
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@/': 'src/',
+        },
+      },
     ],
-  };
-};
+  ],
+});
 
 export default config;
