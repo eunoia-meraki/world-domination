3ef4669b4c0bcce4f8fb5ca040d0c2d16f626ede
Aleksey Koren <alexey.koran@gmail.com>
2022-02-08 15:39:06 +0200

    Fix SignIn

--- webapp/src/views/SignIn/SignIn.tsx
+++ webapp/src/views/SignIn/SignIn.tsx
@@ -61,11 +61,6 @@ export const SignIn: FC = () => (
             Sign In
         </Button>
         <Grid container>
-          <Grid item xs>
-            <Link href="#" variant="body2">
-                Forgot password?
-            </Link>
-          </Grid>
           <Grid item>
             <Link href={Routes.SignUp} variant="body2">
               {'Don\'t have an account? Sign Up'}
