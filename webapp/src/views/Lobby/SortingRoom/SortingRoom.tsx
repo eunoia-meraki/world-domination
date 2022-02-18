23f17436ae91708719144a8cba41517a9552ebc6
ivan-kuricyn <kuritsin020994@gmail.com>
2022-02-19 02:24:32 +0300

    feat: add sorting room table template

--- webapp/src/views/Lobby/SortingRoom/SortingRoom.tsx
+++ webapp/src/views/Lobby/SortingRoom/SortingRoom.tsx
@@ -1,4 +1,16 @@
-import { Box, Button, Container } from '@mui/material';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  Container,
+  TableRow,
+  styled,
+  Button,
+} from '@mui/material';
+import { tableCellClasses } from '@mui/material/TableCell';
 import { useMatch, useNavigate } from 'react-location';
 
 import type { FC } from 'react';
@@ -6,6 +18,66 @@ import type { FC } from 'react';
 import type { LobbyLocation } from '../LobbyLocations';
 
 import { Routes } from '@/enumerations';
+
+const data: {
+  country: string;
+  players: { role: string; login: string }[];
+}[] = [
+  {
+    country: 'Russia',
+    players: [
+      {
+        role: 'President',
+        login: 'Roman',
+      },
+      {
+        role: 'Citizen',
+        login: 'Dima',
+      },
+    ],
+  },
+  {
+    country: 'China',
+    players: [
+      {
+        role: 'President',
+        login: 'Roman',
+      },
+      {
+        role: 'Citizen',
+        login: 'Dima',
+      },
+    ],
+  },
+  {
+    country: 'USA',
+    players: [
+      {
+        role: 'President',
+        login: 'Roman',
+      },
+      {
+        role: 'Citizen',
+        login: 'Dima',
+      },
+    ],
+  },
+];
+
+const HeadTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    fontSize: 14,
+    fontWeight: 700,
+    backgroundColor: theme.palette.grey[400],
+  },
+}));
+
+const BodyTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.body}`]: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
 
 export const SortingRoom: FC = () => {
   const {
@@ -15,22 +87,54 @@ export const SortingRoom: FC = () => {
   const navigate = useNavigate();
 
   const onClick = (): void => {
-    navigate({ to: `${Routes.Lobby}/${gameId}` } );
+    // TODO think
+    // sessionStorage.setItem('currentGameId', gameId);
+    navigate({ to: `${Routes.Lobby}/${gameId}` });
   };
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          mt: '40vh',
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
+    <Container
+      component="main"
+      maxWidth="md"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        flexGrow: 1,
+      }}
+    >
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <Button variant="contained">Start</Button>
         <Button variant="contained" onClick={onClick}>
-          Start
+          Leave
         </Button>
-      </Box>
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                <HeadTableCell>Country</HeadTableCell>
+                <HeadTableCell>Role</HeadTableCell>
+                <HeadTableCell>Player</HeadTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((item, index) => (
+                <>
+                  <TableRow key={index.toString()}>
+                    <BodyTableCell rowSpan={2}>{item.country}</BodyTableCell>
+                    <BodyTableCell>{item.players[0].role}</BodyTableCell>
+                    <BodyTableCell>{item.players[0].login}</BodyTableCell>
+                  </TableRow>
+                  <TableRow>
+                    <BodyTableCell>{item.players[1].role}</BodyTableCell>
+                    <BodyTableCell>{item.players[1].login}</BodyTableCell>
+                  </TableRow>
+                </>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
     </Container>
   );
 };
